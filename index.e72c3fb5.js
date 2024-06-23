a72b912b650d07074e5379f4f3954e1c832d0b68
Nazarii <kostrubitskiiwork@gmail.com>
2024-06-23 21:57:58 +0300

    deploy

---
+++ index.e72c3fb5.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const startButton = document.querySelector(".start-button");
+    const overlay = document.querySelector(".overlay");
+    const gameImage = document.querySelector(".game__image");
+    const gameIframe = document.getElementById("game");
+    startButton.addEventListener("click", function() {
+        overlay.style.display = "none";
+        gameImage.style.display = "none";
+        gameIframe.style.display = "block";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector(".topBar__scroll");
+    const footer = document.querySelector(".footer");
+    button.addEventListener("click", function() {
+        footer.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+//# sourceMappingURL=index.e72c3fb5.js.map
